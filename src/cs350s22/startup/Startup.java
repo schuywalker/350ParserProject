e0474a65eb92e6d7adbe104c7b0c5d0ae4cf1e43
schuyler <sasplin@ewu.edu>
2022-06-01 19:42:06 -0700

    testing parsing. we can delete this code later on - just for getting started.

--- src/cs350s22/startup/Startup.java
+++ src/cs350s22/startup/Startup.java
@@ -4,6 +4,8 @@ import cs350s22.component.ui.parser.A_ParserHelper;
 import cs350s22.component.ui.parser.Parser;
 import cs350s22.component.ui.parser.ParserHelper;
 
+import java.util.Locale;
+
 //=================================================================================================================================================================================
 public class Startup
 {
@@ -22,7 +24,14 @@ public class Startup
       
       // this command must come first. The filenames do not matter here
       startup.parse("@CONFIGURE LOG \"a.txt\" DOT SEQUENCE \"b.txt\" NETWORK \"c.txt\" XML \"d.txt\"");
-      
+
+      // testing - written by schuyler. can be deleted
+      startup.parse("BUILD NETWORK WITH COMPONENT[S] id+");
+      startup.parse("SEND MESSAGE [ids] [groups] POSITION REPORT");
+      startup.parse("CREATE MAPPER id INTERPOLATION (LINEAR | SPLINE) DEFINITION string");
+      startup.parse("@CLOCK SET RATE value");
+
+
       // run your tests like this
       startup.parse("@exit");
    }
@@ -33,7 +42,37 @@ public class Startup
       System.out.println("PARSE> "+ parse);
       
       Parser parser = new Parser(_parserHelper, parse);
-      
+
+      String[] parsedWords = parse.toUpperCase().split(" ");
+
+
+      if (parsedWords[0].charAt(0) == '@'){
+         // its a meta command. see pages 7 and 8
+         System.out.println(parse);
+         switch (parsedWords[0].substring(1).toLowerCase()) {
+            case "exit":
+               System.exit(0);
+         }
+      }
+      else if (parsedWords[0].equalsIgnoreCase("CREATE")){
+         // create new parser
+         System.out.println(parse);
+      }
+      else if (parsedWords[0].equalsIgnoreCase("SEND")){
+         //  SEND MESSAGE PING
+         //  SEND MESSAGE [ids] [groups] POSITION REQUEST value
+         //  SEND MESSAGE [ids] [groups] POSITION REPORT
+         System.out.println(parse);
+      }
+      else if (parsedWords[0].equalsIgnoreCase("BUILD")){
+         //  F. Network Commands:
+         //  BUILD NETWORK WITH COMPONENT[S] id+
+         System.out.println(parse);
+      }
+      else {
+         throw new IllegalArgumentException("bad parse message");
+      }
+
       parser.parse();
    }
 }
