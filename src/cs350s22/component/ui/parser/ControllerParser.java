26080fb11d9cb95b1245920fa036296900bb880e
roydunn19 <roydunn100@gmail.com>
2022-06-05 14:38:41 -0700

    added scaffold to MapperParser class

--- src/cs350s22/component/ui/parser/ControllerParser.java
+++ src/cs350s22/component/ui/parser/ControllerParser.java
@@ -4,28 +4,6 @@ import cs350s22.support.Identifier;
 
 public class ControllerParser {
     public static void controllerCommand(A_ParserHelper parserHelper, String[] commandText) {
-//        for (int i = 2; i < commandText.length; i++)
-//        {
-//            switch (commandText[i])
-//            {
-//                case "forwarding":
-//                    Identifier controllerId = Identifier.make(commandText[3]);
-//                    if (!commandText[4].equals("sensor") && !commandText[4].equals("acceleration"))
-//                    {
-//                        // GroupLayout.Group (add group)
-//                        //ActuatorLinear linearActuator = new ActuatorLinear(id, commandText[4]); (create linear actuator with group)
-//                        i = 4;
-//                    }
-//                    else if (commandText[4] == "sensor")
-//                    {
-//                        // A_Sensor sensor = new A_Sensor(); (creating a sensor)
-//                        i = 4;
-//                    }
-//                    else
-//                    {
-//                        i = 3;
-//                    }
-//            }
-//        }
+//
     }
 }
