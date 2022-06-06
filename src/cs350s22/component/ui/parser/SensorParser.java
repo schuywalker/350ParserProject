50208c692614a795f4e982fe7cf4c2c1bf22e20b
schuyler <sasplin@ewu.edu>
2022-06-05 20:03:28 -0600

    rand commit

--- src/cs350s22/component/ui/parser/SensorParser.java
+++ src/cs350s22/component/ui/parser/SensorParser.java
@@ -1,17 +1,13 @@
 package cs350s22.component.ui.parser;
 
+import cs350s22.component.sensor.A_Sensor;
+import cs350s22.support.Identifier;
+
+import java.util.List;
 import java.util.Locale;
 
 public class SensorParser {
 
-    public enum Type {
-            SPEED,
-            POSITION
-    }
-
-
-    protected Type type;
-    protected String id;
 
 
     public static void sensorCommand(A_ParserHelper parserHelper, String[] commandText){
@@ -34,18 +30,21 @@ SymbolTable<A_Sensor>.
        */
 
 
-        if (commandText[2].toLowerCase().equals("speed")){
-            this.type = Type.SPEED;
-        }
-        else if (commandText[2].toLowerCase().equals("position")){
-            this.type = Type.POSITION;
-        }
-        else {
-            throw new RuntimeException("bad arg H1 speed/ position");
-        }
-        this.id = commandText[3];
+        Identifier id = Identifier.make(commandText[4]);
 
-        // parses and executes CREATE SENSOR
+//        if (commandText.length >= 5 && commandText[4].equals("group")){
+////            List<Identifier> temp = Identifier.makeList(id);
+////            if (parserHelper.getSymbolTableSensor().contains(id)){
+//
+//            // group already exists
+//                A_Sensor sensorTableGroup = parserHelper.getSymbolTableSensor().get(id);
+//                parserHelper.getSymbolTableSensor().add(id, sensorTableGroup);
+////            }
+//
+
+        }
+
+
 
     }
 }
