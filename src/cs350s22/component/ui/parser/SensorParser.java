41e410baa59fa2a5b3b1342404d5c99c194f6409
schuyler <sasplin@ewu.edu>
2022-06-14 20:31:33 -0600

    initial sensor completion

--- src/cs350s22/component/ui/parser/SensorParser.java
+++ src/cs350s22/component/ui/parser/SensorParser.java
@@ -1,6 +1,9 @@
 package cs350s22.component.ui.parser;
 
+import cs350s22.component.A_Component;
 import cs350s22.component.sensor.A_Sensor;
+import cs350s22.component.sensor.mapper.A_Mapper;
+import cs350s22.component.sensor.reporter.A_Reporter;
 import cs350s22.component.sensor.watchdog.A_Watchdog;
 import cs350s22.support.Identifier;
 
@@ -11,7 +14,7 @@ import java.util.Locale;
 /*
 I'm super confused as to where POSITION/ SPEED goes. seems like it'd be an
 ID but then were does the name go (i.e. mySensor1). Does it just by default have
-an id of sensor, all, position/speed, mySensor1, + watchdogs and/or reporters and/or mapper?
+an id of sensor, all, position/speed, mySensor1, + watchdog and/or reporter and/or mapper?
 I must be missing something.
 
 -Schuyler
@@ -25,18 +28,18 @@ public class SensorParser {
 
     public static void sensorCommand(A_ParserHelper parserHelper, String[] commandText){
       /*
-      The sensor command is responsible for creating a sensor with optional reporterss, optional watchdogss, and an optional mapper.
+      The sensor command is responsible for creating a sensor with optional reporters, optional watchdogs, and an optional mapper.
 1. CREATE SENSOR (SPEED | POSITION) id1 [groups] [REPORTER[S] id2+] [WATCHDOG[S] id3+] [MAPPER id4] Creates a sensor that reports either the speed or position of the actuator it monitors. Position comes from the actuator.
 Speed is based on change in position with respect to change in time.
 An optional mapper maps the raw value of the sensor to its mapped value. If no mapper is provided, the mapped value is the same as the raw value.
-Optional reporterss send messages at specified times with the mapped value of the sensor.
-Optional watchdogss verify that the mapped value of the sensor is in compliance.
+Optional reporters send messages at specified times with the mapped value of the sensor.
+Optional watchdogs verify that the mapped value of the sensor is in compliance.
 Example:
 CREATE SENSOR POSITION mySensor8 GROUP myGroup1 REPORTERS myReporter1 MAPPER myMapper1
 CREATE SENSOR POSITION mySensor16 GROUP myGroup1 REPORTERS myReporter1 WATCHDOGS myWatchdog1
   myWatchdog2 MAPPER myMapper1
 Architecture Binding:
-If reporterss, watchdogss, or a mapper are specified, get them from SymbolTable<A_Reporter>,
+If reporters, watchdogs, or a mapper are specified, get them from SymbolTable<A_Reporter>,
 SymbolTable<A_Watchdog>, or SymbolTable<A_Mapper>, respectively.
 Create a new MySensor object with the arguments provided and add it to
 SymbolTable<A_Sensor>.
@@ -58,7 +61,7 @@ SymbolTable<A_Sensor>.
             // make list/ group identifier
             int i = 4;
             groupBuilder = new ArrayList<>();
-            while (i < commandText.length && !commandText[i].equals("reporters") && !commandText[i].equals("watchdogs") && !commandText[i].equals("mapper")){
+            while (i < commandText.length && !commandText[i].equals("reporter") && !commandText[i].equals("watchdog") && !commandText[i].equals("mapper")){
                 groupBuilder.add(Identifier.make(commandText[i]));
                 i++;
             }
@@ -72,59 +75,134 @@ SymbolTable<A_Sensor>.
 
         }
 
-        int watchdogs = -1;
+
+
+        // -1 if not present. array position + 1 if present
+        int watchdog = -1;
         int mapper = -1;
-        int reporters = -1;
+        int reporter = -1;
+        List<Identifier> watchdogList = null;
+        List <Identifier> reporterList = null;
+//        List<Identifier> mapperList = null;
+        Identifier mapperID = null;
+        A_Mapper mapper3 = null;
+        A_Mapper mapper4 = null;
+
         for (int i = 3; i < commandText.length; i++){
-            if (commandText[i].equals("watchdogs")){
-                watchdogs = i;
+            if (commandText[i].equals("watchdog")){
+                watchdog = i+1;
             }
             else if (commandText[i].equals("mapper")){
-                mapper = i;
+                mapper = i+1;
             }
-            else if (commandText[i].equals("reporters")){
-                reporters = i;
+            else if (commandText[i].equals("reporter")){
+                reporter = i+1;
             }
         }
 
-        if (watchdogs > 0){
-            ArrayList<Identifier> watchdogsBuilder = new ArrayList<>();
-            while (watchdogs < commandText.length && !commandText[watchdogs].equals("reporters") && !commandText[watchdogs].equals("watchdogs") && !commandText[watchdogs].equals("mapper")){
-                watchdogsBuilder.add(Identifier.make(commandText[watchdogs]));
-                watchdogs++;
+        if (watchdog > 0){
+            ArrayList<Identifier> watchdogBuilder = new ArrayList<>();
+            while (watchdog < commandText.length && !commandText[watchdog].contains("reporter") && !commandText[watchdog].contains("watchdog") && !commandText[watchdog].equals("mapper")){
+                watchdogBuilder.add(Identifier.make(commandText[watchdog]));
+                watchdog++;
             }
 
-            List<Identifier> watchdogsList = Identifier.makeList(Identifier.list(watchdogsBuilder));
+
+            watchdogList = Identifier.makeList(Identifier.list(watchdogBuilder));
 
         }
 
-        if (reporters > 0) {
-            ArrayList<Identifier> reportersBuilder = new ArrayList<>();
-            while (reporters < commandText.length  && !commandText[reporters].equals("reporters") && !commandText[reporters].equals("watchdogs") && !commandText[reporters].equals("mapper")){
-                reportersBuilder.add(Identifier.make(commandText[reporters]));
-                reporters++;
+        if (reporter > 0) {
+            ArrayList<Identifier> reporterBuilder = new ArrayList<>();
+            while (reporter < commandText.length  && !commandText[reporter].contains("reporter") && !commandText[reporter].contains("watchdog") && !commandText[reporter].equals("mapper")){
+                reporterBuilder.add(Identifier.make(commandText[reporter]));
+                reporter++;
             }
 
-            List <Identifier> reportersList = Identifier.makeList(Identifier.list(reportersBuilder));
+            reporterList = Identifier.makeList(Identifier.list(reporterBuilder));
         }
 
 
 
-        if (mapper > 0){}
-        if (reporters > 0){
+        if (mapper > 0){
+//            ArrayList<Identifier> mappersBuilder = new ArrayList<>();
+//            while (mapper < commandText.length && !commandText[watchdog].contains("reporter") && !commandText[watchdog].contains("watchdog") && !commandText[watchdog].equals("mapper")){
+//                mappersBuilder.add(Identifier.make(commandText[mapper]));
+//                mapper++;
+//            }
+            mapperID = Identifier.make(commandText[mapper]);
+
+//            mapper3 = MapperParser.mapperCommand(parserHelper,
+              mapper4 = parserHelper.getSymbolTableMapper().get(Identifier.make(commandText[mapper]));
+        }
+
+        /*
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor2 GROUP myGroup1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor3 GROUP myGroup1 myGroup2");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor4 MAPPER myMapper1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor5 GROUP myGroup1 REPORTERS myReporter1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor6 GROUP myGroup1 REPORTERS myReporter1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor7 GROUP myGroup1 myGroup2 REPORTERS myReporter1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor8 GROUP myGroup1 REPORTERS myReporter1 MAPPER myMapper1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor9 GROUP myGroup1 WATCHDOG myWatchdog1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor10 GROUP myGroup1 WATCHDOG myWatchdog1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor11 GROUP myGroup1 myGroup2 WATCHDOGS myWatchdog1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor12 GROUP myGroup1 WATCHDOGS myWatchdog1 myWatchdog2 MAPPER myMapper1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor13 GROUP myGroup1 REPORTERS myReporter1 WATCHDOG myWatchdog1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor14 GROUP myGroup1 REPORTERS myReporter1 WATCHDOG myWatchdog1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor15 GROUP myGroup1 myGroup2 REPORTERS myReporter1 WATCHDOGS myWatchdog1");
+        this.parse(parserHelper, "CREATE SENSOR POSITION mySensor16 GROUP myGroup1 REPORTERS myReporter1 WATCHDOGS myWatchdog1 myWatchdog2 MAPPER myMapper1");
+         */
+
+        
+        A_Sensor sensor = null;
+
+
+        if (groupList == null) {
+
+            if (mapper < 0 && watchdog >= 0 && reporter >= 0) {
+
+                sensor = new MySensor(
+                        id,
+                        parserHelper.getSymbolTableReporter().get(reporterList, true),
+                        parserHelper.getSymbolTableWatchdog().get(watchdogList, true)
+                );
+            }
+            else if (mapper >= 0 && watchdog >= 0 && reporter >= 0){
+                sensor = new MySensor(
+                        id,
+                        parserHelper.getSymbolTableReporter().get(reporterList, true),
+                        parserHelper.getSymbolTableWatchdog().get(watchdogList, true),
+                        mapper4
+                );
+            }
+
+        }
+        else {
+
+            if (mapper < 0 && watchdog >= 0 && reporter >= 0) {
+
+                sensor = new MySensor(
+                        id,
+                        parserHelper.getSymbolTableReporter().get(reporterList, true),
+                        parserHelper.getSymbolTableWatchdog().get(watchdogList, true),
+                        groupList
+                );
+            }
+            else if (mapper >= 0 && watchdog >= 0 && reporter >= 0){
+                sensor = new MySensor(
+                        id,
+                        parserHelper.getSymbolTableReporter().get(reporterList, true),
+                        parserHelper.getSymbolTableWatchdog().get(watchdogList, true),
+                        groupList,
+                        mapper4
+                );
+            }
             
         }
         
-        A_Sensor sensor = null;
-        
-        if (watchdogs > 0 && reporters > 0 && groupList == null) {
-            //sensor = new A_Sensor(id, reporters, watchdogs);
-        }
-
-
-
-//            A_Sensor newSensor = new A_Sensor()
-//            parserHelper.getSymbolTableSensor().add(id, newSensor);
+        parserHelper.getSymbolTableSensor().add(id, sensor);
 
     }
 }
